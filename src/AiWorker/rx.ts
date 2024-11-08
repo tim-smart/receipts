1bdf7c73f9b41504534ff828a36082999fe3aab5
Tim <hello@timsmart.co>
2024-11-08 23:26:54 +0100

    keep worker alive

--- src/AiWorker/rx.ts
+++ src/AiWorker/rx.ts
@@ -2,13 +2,17 @@ import { OpenAiCreds } from "@/Ai"
 import { AiWorkerLive, JazzAccount } from "@/AiWorker"
 import { ReceiptsAccount } from "@/Domain/Account"
 import { Rx } from "@effect-rx/rx-react"
-import { Data, Layer, Redacted } from "effect"
+import { Data, Hash, Layer, Redacted } from "effect"
 
 export class AiWorkerConfig extends Data.Class<{
   readonly account: ReceiptsAccount
   readonly openaiApiKey: Redacted.Redacted
   readonly openaiModel: string
-}> {}
+}> {
+  [Hash.symbol]() {
+    return Hash.string(Redacted.value(this.openaiApiKey))
+  }
+}
 
 export const aiWorkerRx = Rx.family((config: AiWorkerConfig) =>
   Rx.runtime(
@@ -21,5 +25,5 @@ export const aiWorkerRx = Rx.family((config: AiWorkerConfig) =>
       ),
       Layer.provide(Layer.succeed(JazzAccount, config.account)),
     ),
-  ),
+  ).pipe(Rx.setIdleTTL("10 seconds")),
 )
