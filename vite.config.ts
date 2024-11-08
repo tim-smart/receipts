c5463114996cb89fd116e24036a0ff4919a31149
Tim <hello@timsmart.co>
2024-11-08 16:07:02 +0100

    revert base change

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/receipts/",
   plugins: [
     TanStackRouterVite(),
     react(),
